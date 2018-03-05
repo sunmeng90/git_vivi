9c78f4aa63b8c6cf69985fbe8b51c6e59fd3a906
Meng Sun <sunmeng90@126.com>
2018-03-05 22:16:33 +0800

    feature greeting

--- src/org/vivi/demo/Demo.java
+++ src/org/vivi/demo/Demo.java
@@ -5,6 +5,11 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git");
+		printGreeting("vivi");
+	}
+	
+	public static void printGreeting(String name) {
+		System.out.println("Gretting "+ name);
 	}
 
 }
